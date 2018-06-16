87f30627d1aa30d011cee5e5da39c1e5c0d0c6b3
veeqtor <nwokeochavictor@gmail.com>
2018-06-16 22:18:38 +0100

    ch(env-variables): Add .env.sample

--- client/js/userReqDetails.js
+++ client/js/userReqDetails.js
@@ -1,7 +1,7 @@
 const urlString = window.location.href;
 const url = new URL(urlString);
 const param = url.searchParams.get('id');
-const token = JSON.parse(sessionStorage.getItem('token'));
+const token = JSON.parse(localStorage.getItem('token'));
 const apiUrl = `/api/v1/users/requests/${param}`;
 const apiUrl2 = '/api/v1/users/requests';
 const reqDetails = document.getElementById('user-details');
@@ -18,7 +18,6 @@ if (token && token.auth) {
   })
     .then(res => res.json())
     .then((data) => {
-      // console.log(data);
       if (data.error) {
         alertBox.innerHTML = `
     <header>
@@ -51,17 +50,43 @@ if (token && token.auth) {
     .then(res => res.json())
     .then((result) => {
       const output = `
-                <li><strong>Creator:</strong>&nbsp;${result.result[0].requester_name}</li>
-                <li><strong>Email of creator:</strong>&nbsp;${result.result[0].requester_email}</li>
-                <li><strong>Date created:</strong>&nbsp;${result.result[0].date}</li>
-                <li><strong>Department:</strong>&nbsp;${result.result[0].dept}</li>
-                <li><strong>Status:&nbsp;</strong>${result.result[0].status}</li>
-                <li><strong>Request details:&nbsp;</strong>${result.result[0].request}</li>
-       
-      `;
+                      <tr>
+                          <td><i>Created&nbsp;by:</i></td>
+                          <td>${result.result[0].requester_name}</td>
+                      </tr>
+                      <tr>
+                          <td><i>Email:</i></td>
+                          <td>${result.result[0].requester_email}</td>
+                      </tr>
+                      <tr>
+                          <td><i>Date&nbsp;created:</i></td>
+                          <td>${result.result[0].date}</td>
+                      </tr>
+                      <tr>
+                          <td><i>Department:</i></td>
+                          <td>${result.result[0].dept}</td>
+                      </tr>
+                      <tr>
+                          <td><i>Status:</i></td>
+                          <td class="${result.result[0].status}">${result.result[0].status}</td>
+                      </tr>
+                      <tr>
+                          <td><i>Request:</i></td>
+                          <td>
+                              <p>${result.result[0].request}</p>
+                          </td>
+                      </tr>`;
       const output2 = ` 
-                <a href="edit-request.html?id=${result.result[0].id}" class="btn btn-default" title="Edit">Edit</a>
-                <a href="user.html" title="Go back" class="btn btn-default">Back</a>
+                    <div>
+                       <a href="user.html" class="left btn btn-primary" title="Click to edit request"><i
+                                class="fa fa-arrow-left"></i> Back</a>
+                    </div>
+                    <div>
+                        <a href="edit-request.html?id=${result.result[0].id}" class="right btn btn-primary" disabled
+                           title="Click to edit request"><i
+                                class="fa fa-edit"></i> Edit</a>
+                    </div>
+                    <div class="clearfix"></div>
       `;
 
       reqBtn.innerHTML = output2;
